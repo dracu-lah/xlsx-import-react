02891ca1088c11dda724df568dd64d8e1ce02ab8
dracu-lah <110589548+dracu-lah@users.noreply.github.com>
2023-03-28 02:57:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { read, utils } from "xlsx";
+
+export default function App() {
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = utils.sheet_to_json(worksheet);
+        console.log(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form>
+        <label htmlFor="upload">Upload File</label>
+        <input
+          type="file"
+          name="upload"
+          id="upload"
+          onChange={readUploadFile}
+        />
+      </form>
+    </div>
+  );
+}
